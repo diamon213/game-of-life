02c2538bc8ff8f2b24c9b21378c16bdde2070cec
diamon213 <diamon213@hotmail.com>
2020-10-09 18:06:04 +0200

    randomizes value of cell at start

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+extern crate js_sys;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -110,7 +111,7 @@ impl Universe {
 
         let cells = (0..width * height)
             .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
+                if js_sys::Math::random() < 0.5 {
                     Cell::Alive
                 } else {
                     Cell::Dead
